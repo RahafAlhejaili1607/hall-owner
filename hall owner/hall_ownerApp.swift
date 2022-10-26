011de238af839202037f0cc9942bc97d71cd0c38
rahaf <rahaf1706@icloud.com>
2022-10-26 09:26:24 +0300

    Initial Commit

---
+++ hall owner/hall_ownerApp.swift
@@ -0,0 +1,17 @@
+//
+//  hall_ownerApp.swift
+//  hall owner
+//
+//  Created by Rahaf Alhejaili on 01/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct hall_ownerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
