25c4d002bcfa2f627fe1578cb08c56b088010d6a
layan <layanald83@gmail.com>
2022-10-31 08:27:03 +0300

    Initial Commit

---
+++ NEW/NEWApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEWApp.swift
+//  NEW
+//
+//  Created by layan aldhahri on 06/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct NEWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
